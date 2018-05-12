8e4a05ae0255147779f2920b072dc75d9b178359
DimGun <zhur85@gmail.com>
2018-05-12 21:30:03 +0300

    WIP: Added Stop and Play buttons
    there is an issue with Mic.End and Mic.IsRecording methods

--- Assets/SaveWave.cs
+++ Assets/SaveWave.cs
@@ -6,25 +6,41 @@ public class SaveWave : MonoBehaviour {
 
     AudioClip myAudioClip;
 
-    void Start () { }
-    void Update () { }
-    void OnGUI () {
+    void Start() { }
+    void Update() { }
+    void OnGUI() {
         GUILayout.BeginArea(new Rect(0, 0, 100, 100));
 
-        if (GUILayout.Button ("Record")) {
-            myAudioClip = Microphone.Start (null, false, 10, 44100);
+        bool isRecording = !Microphone.IsRecording();
+
+        if (!isRecording) {
+            if (GUILayout.Button("Record")) {
+                myAudioClip = Microphone.Start(null, false, 10, 44100);
+            }
+
+            if (myAudioClip) {
+                if (GUILayout.Button("Play")) {
+                    AudioSource audioSource = GetComponent<AudioSource>();
+                    audioSource.clip = myAudioClip;
+                    audioSource.Play();
+                }
+
+                if (GUILayout.Button("Save")) {
+                    for (int i = 1;; i++)
+                        if (System.IO.File.Exists("myfile")) {
+                            VoiceRecord.Save("myfile" + i, myAudioClip);
+                        }
+                    else {
+                        VoiceRecord.Save("myfile", myAudioClip);
+
+                    }
+                }
+            }
         }
 
-        if (GUI.Button (new Rect (10, 70, 60, 50), "Save")) {
-
-        if (GUILayout.Button ("Save")) {
-            for (int i = 1;; i++)
-                if (System.IO.File.Exists ("myfile")) {
-                    VoiceRecord.Save ("myfile" + i, myAudioClip);
-                }
-            else {
-                VoiceRecord.Save ("myfile", myAudioClip);
-                //                      audio.Play();
+        if (isRecording) {
+            if (GUILayout.Button("Stop Record")) {
+                Microphone.End();
             }
         }
 
