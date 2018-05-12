d08644e71e4f9fd7a3e6e87c9dcc09fd71d8eb53
DimGun <zhur85@gmail.com>
2018-05-12 20:54:47 +0300

    Fixed outline of SaveWave

--- Assets/SaveWave.cs
+++ Assets/SaveWave.cs
@@ -2,33 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
- public class SaveWave : MonoBehaviour 
- {
-  
- AudioClip myAudioClip; 
-  
- void Start() {}
- void Update () {}
- void OnGUI()
-            {
-         if (GUI.Button(new Rect(10,10,60,50),"Record"))
-             { 
-             myAudioClip = Microphone.Start ( null, false, 10, 44100 );
-             }
- 
-         if (GUI.Button(new Rect(10,70,60,50),"Save"))
-         {
-             
-             for (int i = 1; ;i++)
-                 if (System.IO.File.Exists("myfile"))
-                       {
-                     SavWav.Save("myfile" + i, myAudioClip);    
-                     }        
-                 else
-                     {
-                     SavWav.Save("myfile", myAudioClip); 
- //                      audio.Play();
-                     }
+public class SaveWave : MonoBehaviour {
+
+    AudioClip myAudioClip;
+
+    void Start () { }
+    void Update () { }
+    void OnGUI () {
+        if (GUI.Button (new Rect (10, 10, 60, 50), "Record")) {
+            myAudioClip = Microphone.Start (null, false, 10, 44100);
+        }
+
+        if (GUI.Button (new Rect (10, 70, 60, 50), "Save")) {
+
+            for (int i = 1;; i++)
+                if (System.IO.File.Exists ("myfile")) {
+                    SavWav.Save ("myfile" + i, myAudioClip);
+                }
+            else {
+                SavWav.Save ("myfile", myAudioClip);
+                //                      audio.Play();
             }
-     }
- }
+        }
+    }
+}
