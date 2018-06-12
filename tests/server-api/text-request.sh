155358314c6bbc5a91b3234bba48d9b12e441dac
DimGun <zhur85@gmail.com>
2018-06-12 21:03:57 +0300

    Added examples of server requests

---
+++ tests/server-api/text-request.sh
@@ -0,0 +1,5 @@
+curl -v -X POST \
+  https://api.ht.studsib.ru/text-request \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'content-type: multipart/form-data' \
+  -F text='Кто ты?'
---
+++ tests/server-api/voice-request.sh
@@ -0,0 +1,7 @@
+curl -v -X POST \
+  https://api.ht.studsib.ru/voice-request \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'content-type: multipart/form-data' \
+  -F text='Кто ты?'
+#  -F voice=@/Users/dimgun/Documents/Projects/Home/HoloGirl/tests/server-api/12345-bunny.wav
+
