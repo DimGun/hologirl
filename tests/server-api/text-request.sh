494810136e1ae555ba7bc723a2ea7c8261467373
DimGun <zhur85@gmail.com>
2018-07-15 19:36:51 +0300

    Fixed unit tests for backend part

--- tests/server-api/text-request.sh
+++ tests/server-api/text-request.sh
@@ -1,5 +1,4 @@
 curl -v -X POST \
   https://api.ht.studsib.ru/text-request \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'content-type: multipart/form-data' \
+  -H 'Content-Type: multipart/form-data' \
   -F text='Кто ты?'
--- tests/server-api/voice-request.sh
+++ tests/server-api/voice-request.sh
@@ -1,7 +1,5 @@
 curl -v -X POST \
   https://api.ht.studsib.ru/voice-request \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'content-type: multipart/form-data' \
-  -F text='Кто ты?'
-#  -F voice=@/Users/dimgun/Documents/Projects/Home/HoloGirl/tests/server-api/12345-bunny.wav
+  -H 'Content-Type: multipart/form-data' \
+  -F voice=@./12345-bunny.wav
 
