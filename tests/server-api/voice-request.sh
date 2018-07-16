ccf162365ea0a680d3b589345fcb92872ddf6592
DimGun <zhur85@gmail.com>
2018-07-16 18:21:54 +0300

    Added handling of voice response readiness to the test

--- tests/server-api/voice-request.sh
+++ tests/server-api/voice-request.sh
@@ -1,6 +1,7 @@
 set -e
 
 HOST=https://ht.studsib.ru
+MAX_ATTEMPTS=3
 
 voice_request_url=$HOST/voice-request
 echo  "01. Sending request to $voice_request_url"
@@ -15,17 +16,39 @@ echo 'Repsonse:'
 jq "." 01-voice-request-response.json
 echo "--------------------------"
 
+
 metadata_url=`jq ".data.metadata" 01-voice-request-response.json | tr -d '"'`
 metadata_url="$HOST$metadata_url"
 
-echo "02. Fetching metadata from $metadata_url"
-curl \
-  -X GET "$metadata_url" \
-  -o 02-metadata-response.json \
-  --silent
-echo 'Repsonse:'
-jq "." 02-metadata-response.json
-echo "--------------------------"
+attempt=1
+while true; do
+  echo "02. Fetching metadata from $metadata_url. Attempt #$attempt"
+
+  curl \
+    -X GET "$metadata_url" \
+    -o 02-metadata-response.json \
+    --silent
+  echo 'Repsonse:'
+  jq "." 02-metadata-response.json
+
+  # Check response readiness
+  is_voice_ready=`jq ".data.voice_ready" 02-metadata-response.json`
+  if [ $is_voice_ready == 'true' ]; then
+    echo "Voice response is ready"
+    break
+  else
+    echo "Voice response is not ready."
+    if [ $attempt -lt $MAX_ATTEMPTS ]; then
+      echo "Making a new attempt"
+      ((attempt++))
+      sleep 1
+    else
+      echo "Failed to get voice response in $attempt attempts. Giving up."
+      exit 1
+    fi
+  fi
+  echo "--------------------------"
+done
 
 voice_url=`jq ".data.voice" 01-voice-request-response.json | tr -d '"'`
 voice_url="$HOST$voice_url"
