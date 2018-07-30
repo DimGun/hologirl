6956a12db1115c9f055c7bfa653ebb40b6765985
DimGun <zhur85@gmail.com>
2018-07-30 20:49:11 +0300

    Added parsing of voice and metadata uris from response

--- Assets/Scripts/VoiceControl.cs
+++ Assets/Scripts/VoiceControl.cs
@@ -103,61 +103,89 @@ public class VoiceControl : MonoBehaviour {
         const string host = "http://localhost:8080";
         //const string host = "https://ht.studsib.ru";
 
-        UnityWebRequest voiceRequest = CreateVoiceWebRequest(host + "/voice-request", wavFileData);
-        yield return voiceRequest.SendWebRequest();
-        LogTextWebRequest(voiceRequest);
+        // Send request
+        string metadataUri = null;
+        string voiceAnswerUri = null;
+        {
+            string voiceRequestUri = "/voice-request";
+            UnityWebRequest voiceRequest = CreateVoiceWebRequest(host + voiceRequestUri, wavFileData);
+            yield return voiceRequest.SendWebRequest();
+            LogTextWebRequest(voiceRequest);
 
-        byte attemptsLeft = 3;
-        bool voiceReady = false;
-        JSONNode data = null;
-
-        // Ask for answer for 3 times
-        while(attemptsLeft > 0 && !voiceReady) {
-            Debug.Log("Getting metadata. Attempts left: " + attemptsLeft);
-            // Give server some time to process request
-            yield return new WaitForSeconds(1.0f);
-
-            // Request for metadata
-            UnityWebRequest metadataRequest = UnityWebRequest.Get("https://ht.studsib.ru/response/oOGpVY/metadata");
-            yield return metadataRequest.SendWebRequest();
-            LogTextWebRequest(metadataRequest);
-
-            // Parse metadata to find if voice answer is ready
-            string jsonResponseStr = metadataRequest.downloadHandler.text;
-            if (jsonResponseStr != null && jsonResponseStr.Length > 0) {
+            // Parse response to get voice and metadata urls
+            string jsonResponseStr = voiceRequest.downloadHandler.text;
+            if (!string.IsNullOrEmpty(jsonResponseStr)) {
                 JSONNode json = JSON.Parse(jsonResponseStr);
                 if (json != null) {
-                    data = json["data"];
-                    voiceReady = data["voice_ready"].AsBool;
+                    metadataUri = json["data"]["metadata"];
+                    voiceAnswerUri = json["data"]["voice"];
                 }
             }
-            attemptsLeft--;
         }
 
-        if (data) {
-            string question = data["question"]? data["question"].Value : "¯\\_(ツ)_/¯";
-            Debug.Log("@user> " + question);
-            Debug.Log("@alla> " + data["answer"].Value);
+        if(string.IsNullOrEmpty(metadataUri)) {
+            Debug.Log("Failed to get metadata");
+            yield break;
         }
 
-        if (!voiceReady) {
+        // Get metadata
+        bool voiceReady = false;
+        string answerText = null;
+        string questionText = null;
+        {
+            byte attemptsLeft = 3;
+            JSONNode data = null;
+
+            // Ask for answer for 3 times
+            while(attemptsLeft > 0 && !voiceReady) {
+                Debug.Log("Getting metadata. Attempts left: " + attemptsLeft);
+                // Give server some time to process request
+                yield return new WaitForSeconds(1.0f);
+
+                UnityWebRequest metadataRequest = UnityWebRequest.Get(host + metadataUri);
+                yield return metadataRequest.SendWebRequest();
+                LogTextWebRequest(metadataRequest);
+
+                // Parse metadata to find if voice answer is ready
+                string jsonResponseStr = metadataRequest.downloadHandler.text;
+                if (jsonResponseStr != null && jsonResponseStr.Length > 0) {
+                    JSONNode json = JSON.Parse(jsonResponseStr);
+                    if (json != null) {
+                        data = json["data"];
+                        voiceReady = data["voice_ready"].AsBool;
+                        questionText = data["question"].Value;
+                        answerText = data["answer"].Value;
+                    }
+                }
+                attemptsLeft--;
+            }
+        }
+
+        if (!string.IsNullOrEmpty(answerText) || !string.IsNullOrEmpty(questionText)) {
+            string formatttedQuestion = string.IsNullOrEmpty(questionText) ? "¯\\_(ツ)_/¯" : questionText;
+            Debug.Log("@user> " + formatttedQuestion);
+            Debug.Log("@alla> " + answerText);
+        }
+
+        if (!voiceReady || string.IsNullOrEmpty(voiceAnswerUri)) {
             Debug.Log("Server failed to prepare a voice answer.");
             yield break;
         }
 
         // Download voice answer
-        string voiceAnswerUrl = "https://ht.studsib.ru/response/oOGpVY/voice";
-        UnityWebRequest voiceAnswerRequest = UnityWebRequest.Get(voiceAnswerUrl);
-        DownloadHandlerAudioClip voiceAnswerDownloadHandler = new DownloadHandlerAudioClip(voiceAnswerUrl, AudioType.WAV);
-        voiceAnswerRequest.downloadHandler = voiceAnswerDownloadHandler;
-        yield return voiceAnswerRequest.SendWebRequest();
-        LogBinaryWebRequest(voiceAnswerRequest);
-        
-        if (!voiceAnswerRequest.isNetworkError && !voiceAnswerRequest.isHttpError) {
-            audioSource.clip = voiceAnswerDownloadHandler.audioClip;
-            audioSource.Play();
-        } else {
-            Debug.Log("Failed to play voice answer");
+        {
+            UnityWebRequest voiceAnswerRequest = UnityWebRequest.Get(host + voiceAnswerUri);
+            DownloadHandlerAudioClip voiceAnswerDownloadHandler = new DownloadHandlerAudioClip(voiceAnswerUri, AudioType.WAV);
+            voiceAnswerRequest.downloadHandler = voiceAnswerDownloadHandler;
+            yield return voiceAnswerRequest.SendWebRequest();
+            LogBinaryWebRequest(voiceAnswerRequest);
+
+            if (!voiceAnswerRequest.isNetworkError && !voiceAnswerRequest.isHttpError) {
+                audioSource.clip = voiceAnswerDownloadHandler.audioClip;
+                audioSource.Play();
+            } else {
+                Debug.Log("Failed to play voice answer");
+            }
         }
     }
 
